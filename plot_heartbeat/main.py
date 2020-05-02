e1761032e7715faafd2d314a9ee591bc9db95d75
TsubasaNishihara <63089960+TsubasaNishihara@users.noreply.github.com>
2020-05-02 23:11:49 +0900

    Update plot_heartbeat

--- plot_heartbeat/main.py
+++ plot_heartbeat/main.py
@@ -1,6 +1,7 @@
 # 心拍データをファイルから読み取り，Matplotlibでグラフにプロット
 
 import matplotlib.pyplot as plt
+import numpy as np
 import csv
 import datetime
 
@@ -17,19 +18,27 @@ for line in data_org[1:]:
     data_org[i][1] = int(line[1])
     i += 1
 
-# 2分間隔のデータを生成
-t = 0
-data_plot = [[t], [data_org[1][1]]]
-time = data_org[1][0]
+# グラフにプロットするデータを生成
+time = 0
+data_plot = [[[time], [data_org[1][1]]]]
+init_time = 3600 * data_org[1][0].hour + 60 * data_org[1][0].minute + data_org[1][0].second + 0.000001 * float(data_org[1][0].microsecond)
+i = 0
 for line in data_org[2:]:
-    if (line[0].hour == time.hour) & (line[0].minute == time.minute + 2) & (line[0].second == time.second):
-        t += 2
-        time = line[0]
-        data_plot[0].append(t)
-        data_plot[1].append(line[1])
+    nowtime = 3600 * line[0].hour + 60 * line[0].minute + line[0].second + 0.000001 * float(line[0].microsecond)
+    time = nowtime - init_time
+    if int(time) / (i + 1) == 120:  # 2分(=120秒)ごとに区切る
+        i += 1
+        data_plot.append([[time], [line[1]]])
+    else:
+        data_plot[i][0].append(time)
+        data_plot[i][1].append(line[1])
 
 # グラフにプロット
-plt.plot(data_plot[0], data_plot[1])
-plt.title("Heartbeat")
-plt.xlabel("Time t[min]")
+for j in range(i):
+    plt.figure(j)
+    plt.plot(data_plot[j][0], data_plot[j][1])
+    plt.title("Heartbeat")
+    plt.xlabel("Time t[s]")
+
 plt.show()
+
